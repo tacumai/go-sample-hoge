369b41f7c1ba998f7173a01480281009c0ad4549
tacumai <tacuma.igei@gmail.com>
2021-02-13 15:31:01 +0900

    ver up

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "HOGE: Hello,World!")
+		c.String(200, "HOGE 2!!!!!!!!: Hello,World!")
 	})
 	r.Run()
 }
